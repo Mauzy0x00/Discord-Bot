b222c2e93b9c492b084cd34b78f5791f5953a322
Mauzy <Mauzy.Mail@proton.me>
2023-03-11 20:37:46 -0500

    Hug command implementation (v0.1)

--- commands/hug.js
+++ commands/hug.js
@@ -1,11 +1,26 @@
+const axios = require('axios');     // axios package to make a GET request to the Tenor API and retrieve a gif
 const { SlashCommandBuilder } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
+const { tenorAPI } = require('./config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('hug')
     .setDescription('Give someone a hug :)')
-    .addIntegerOption(option => option.setName('hugee').setDescription('Person to hug')),
+    .addUserOption(option => option.setName('hugee').setDescription('Person to hug').setRequired(true)),
+
     async execute(interaction) {
-        // Your code goes here
+        const query = 'anime-hug';
+        const limit = 1;
+        const mediaFilter = 'minimal';
+        const user = interaction.options.getUser('hugee');
+
+        const { data } = await axios.get(`https://api.tenor.com/v1/search?q=${query}&key=${tenorAPI}&limit=${limit}&media_filter=${mediaFilter}`);
+
+        const hugEmbed = new MessageEmbed()
+        .setDescription(`Sending a hug to ${hugee} ❤️`)
+        .setImage(data.results[0].media[0].gif.url);
+
+        await interaction.reply({ embeds: [hugEmbed] });
     },
 };
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -19,6 +19,7 @@ module.exports = {
 			await command.execute(interaction);
 		}
 		catch (error) {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 			console.error(`Error executing ${interaction.commandName}`);
 			console.error(error);
 		}
