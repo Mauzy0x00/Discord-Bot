d70a2b0e4d7cf5566a550620ca167051c8111414
Mauzy0x <mauzy.mail@proton.me>
2023-05-09 13:57:58 -0400

    Error handling

--- commands/ChatGPT.js
+++ commands/ChatGPT.js
@@ -15,44 +15,48 @@ module.exports = {
         const prompt = interaction.options.getString('prompt');
         console.log(prompt);
 
-        await interaction.deferReply();  // tell discord to wait 15 min 
+        try {
+          await interaction.deferReply();  // tell discord to wait 15 min 
 
-        // Make request to OpenAI
-        const configuration = new Configuration({
-            apiKey: OpenAIApiKey,
-          });
-        const openai = new OpenAIApi(configuration);
+          // Make request to OpenAI
+          const configuration = new Configuration({
+              apiKey: OpenAIApiKey,
+            });
+          const openai = new OpenAIApi(configuration);
 
-        // Retreive language model and create completion
-        const completion = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: [{role: "user", content: `\`${prompt}\``}],
-          });
-        
-        response  = completion.data.choices[0].message.content;
-        response = response.replace(/\n\n/, " ");    // message content from ChatGPT returns with two new lines, replace that with "ChatGPT: "
-        console.log(response);
-
-        const sizeCheck = `${interaction.user.username}: ${prompt} \n\n ChatGPT: ${response}`;
-        
-        // Discord can only send messages that contain less than 200 characters. Check this before sending. 
-        if (sizeCheck.length > 2000){
-          // create .txt file. send txt file and delete from server
-          // if cannot delete txt from java script, have javascript call a bash file
-
-          // Create the file
-          const fileName = 'response.txt';
-          const fileContent = response; 
-          createTextFile(fileName, fileContent);
-
-          // Send the file
-          const attachmentPath = path.join(process.cwd(), 'send_files', fileName);
-          const attachment = new AttachmentBuilder(attachmentPath);
+          // Retreive language model and create completion
+          const completion = await openai.createChatCompletion({
+              model: "gpt-3.5-turbo",
+              messages: [{role: "user", content: `\`${prompt}\``}],
+            });
           
-          interaction.editReply({ files: [attachment] });
+          response  = completion.data.choices[0].message.content;
+          response = response.replace(/\n\n/, " ");    // message content from ChatGPT returns with two new lines, replace that with "ChatGPT: "
+          console.log(response);
 
-        } else {
-          await interaction.editReply({ content: `${interaction.user.username}: \`${prompt}\` \n\n ChatGPT:\`${response}\``});
+          const sizeCheck = `${interaction.user.username}: ${prompt} \n\n ChatGPT: ${response}`;
+          
+          // Discord can only send messages that contain less than 200 characters. Check this before sending. 
+          if (sizeCheck.length > 2000){
+            // create .txt file. send txt file and delete from server
+            // if cannot delete txt from java script, have javascript call a bash file
+
+            // Create the file
+            const fileName = 'response.txt';
+            const fileContent = response; 
+            createTextFile(fileName, fileContent);
+
+            // Send the file
+            const attachmentPath = path.join(process.cwd(), 'send_files', fileName);
+            const attachment = new AttachmentBuilder(attachmentPath);
+            
+            interaction.editReply({ files: [attachment] });
+
+          } else {
+            await interaction.editReply({ content: `${interaction.user.username}: \`${prompt}\` \n\n ChatGPT:\`${response}\``});
+          }
+        } catch(Error) {
+          console.error(Error);
         }
     },
 }; // end main
